0f56d7b848c068d42736fdd407ddbb91fb4eef4e
Victor Sage <victorsage@Yanns-MacBook-Pro.local>
2019-05-02 09:22:36 +0200

    First commit

---
+++ lib/prompt_dialog.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+Future<String> asyncInputDialog(BuildContext context) async {
+  String teamName = '';
+  return showDialog<String>(
+    context: context,
+    barrierDismissible: false, // dialog is dismissible with a tap on the barrier
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('Prénom du participant'),
+        content: new Row(
+          children: <Widget>[
+            new Expanded(
+                child: new TextField(
+              autofocus: true,
+              decoration: new InputDecoration(
+                  labelText: 'Nom', hintText: 'Pedro'),
+              onChanged: (value) {
+                teamName = value;
+              },
+            ))
+          ],
+        ),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('Ok'),
+            onPressed: () {
+              Navigator.of(context).pop(teamName);
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
+
